7c7c8c3707a40d72ce39fdce264ca106f72be33b
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:25:39 +0100

    Add project files.

---
+++ mod2_les8_task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int primeNumbersCount = 0;
+
+        for (int i = 2; i <= 100; i++)
+        {
+            if (isPrime(i))
+            {
+                primeNumbersCount++;
+
+            }
+        }
+
+        Console.WriteLine($"the number of prime numbers in the range is: {primeNumbersCount}");
+
+    }
+    static bool isPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+
+            }
+        }
+        return true;
+    }
+}
